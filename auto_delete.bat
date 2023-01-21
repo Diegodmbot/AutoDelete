7fc60f893a83cecba8d73c5880035d78abb174ba
Diego Díaz Morón <58338943+Diegodm35@users.noreply.github.com>
2023-01-21 10:21:33 +0000

    Script finalizado

--- auto_delete.bat
+++ auto_delete.bat
@@ -1,5 +1,6 @@
 @echo off
-	attrib -r -s -h "C:\Users\Diegodm\Downloads\*.*" /s /d
-	forfiles -p "C:\Users\Diegodm\Downloads" -s -d -30 -c "cmd /c del /q @path"
-	forfiles -p "C:\Users\Diegodm\Downloads" -s -d -30 -c "cmd /c if @isdir==TRUE rd /s /q @path"
+	set ruta=ruta\directorio\objetivo
+	attrib -r -s -h "%ruta%\*.*" /s /d
+	forfiles -p "%ruta%" -s -d -30 -c "cmd /c del /q @path"
+	forfiles -p "%ruta%" -s -d -30 -c "cmd /c if @isdir==TRUE rd /s /q @path"
 end
